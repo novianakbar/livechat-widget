5e126576e6d98e8b883e91b55166d45949116cd7
Novian Akbar <akbarian555@gmail.com>
2025-07-25 15:05:07 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   define: {
-    "process.env": {},
+    "process.env.NODE_ENV": JSON.stringify("production"),
   },
   build: {
     lib: {
